42e6792d5c25f5ed4b087d90810df5854038d0a8
TyrellMartens <tyrell.martens@uleth.ca>
2022-11-29 16:09:42 -0700

    Trying to figure out how to get a circle somewhere on the screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,45 @@
 
+var hoverText = "<h2>Hover over to the circle.</h2>"
+var visibleMaze = false;
+
+//main 
+
+
+//$("#text").append(hoverText);
+//$("#text").children("h2").remove();
+var left = document.getElementById('start').offsetLeft;
+console.log(left);
+
+//functions
+function start() {
+    $("#text").append(hoverText);
+    $("#start").html("<src img='../imgs/circle.svg'>")
+}
+
+function hoverCircle(idLocation) {
+    if (visibleMaze == true) {
+        console.log("hoverCircle called when maze visible")
+    }
+    else {
+        $("#text").append(hoverText);
+    }
+}
+
+
+
+
+
+
 //$(".maze").on("mouseover", gameOver());
+
 //$(".questionMark").on("mouseover", question());
-// $(".questionMark").on("mouseover", funciton() {
-//     console.log(questionNum);
+$(".questionMark").on("mouseover", function () {
+    console.log(this.id);
+}); //this console logs the Id of the question marks
+
+// var visibleMaze = true;
+// $(document).on("click", function () {
+//     $(".img-maze").css("visibility", "visible");
+//     $(".img-maze").css("visibility", "hidden");
 // });
+
