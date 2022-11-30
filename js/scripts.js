38a442a519b33706fc3c1ec366f275f19ad9ebf0
TyrellMartens <tyrell.martens@uleth.ca>
2022-11-29 20:57:58 -0700

    Hover circle works, working on questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,63 @@
 
-var hoverText = "<h2>Hover over to the circle.</h2>"
+var hoverText = "<h2>Hover over to the circle to continue hacking</h2>"
 var visibleMaze = false;
 
 //main 
-
-
-//$("#text").append(hoverText);
-//$("#text").children("h2").remove();
-var left = document.getElementById('start').offsetLeft;
-console.log(left);
+hoverCircle("start");
+$(".questionMark").on("mouseover", question);
 
 //functions
-function start() {
-    $("#text").append(hoverText);
-    $("#start").html("<src img='../imgs/circle.svg'>")
-}
 
-function hoverCircle(idLocation) {
+function hoverCircle(idName) {
     if (visibleMaze == true) {
-        console.log("hoverCircle called when maze visible")
+        console.log("hoverCircle called when maze visible");
     }
     else {
+        var elementPos = document.getElementById(idName).getBoundingClientRect();
         $("#text").append(hoverText);
+
+        $("#content").after("<img src='../imgs/circle.svg' alt='Circle Image' id='circle-img' class='circle'>");
+
+        var translationLeft = elementPos.left + 8 + "px";
+
+        if (idName[0] == 'Q') {
+            var translationTop = elementPos.top + 20 + "px";
+        } else {
+            var translationTop = elementPos.top - 10 + "px";
+        }
+
+        $(".circle").css("left", translationLeft);
+        $(".circle").css("top", translationTop);
+
+        $(".circle").on("mouseover", function () {
+            $("#circle-img").remove();
+            $(".img-maze").css("visibility", "visible");
+            visibleMaze = true;
+            $("#text h2").remove();
+        });
     }
+}
+
+function question() {
+    var idName = this.id;
+    var puzzleWord;
+
+    console.log(idName);
+    $(".img-maze").css("visibility", "hidden");
+    visibleMaze = false;
+
+    if (idName[1] == 2) {
+        puzzleWord = "virus"
+    }
+    else if (idName[1] == 3 || idName[1] == 4) {
+        puzzleWord = (Math.random() + 1).toString(36).substring(8);
+    }
+    else {
+        puzzleWord = (Math.random() + 1).toString(36).substring(7);
+    }
+
+
+
 }
 
 
@@ -30,12 +65,9 @@ function hoverCircle(idLocation) {
 
 
 
-//$(".maze").on("mouseover", gameOver());
-
-//$(".questionMark").on("mouseover", question());
-$(".questionMark").on("mouseover", function () {
-    console.log(this.id);
-}); //this console logs the Id of the question marks
+$(".maze").on("mouseover", function () {
+    console.log("Touching");
+});
 
 // var visibleMaze = true;
 // $(document).on("click", function () {
