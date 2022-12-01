99554c5f07cf7fb05819d1c9eddd9a6ccb1ee30a
TyrellMartens <tyrell.martens@uleth.ca>
2022-11-30 21:18:38 -0700

    UX design added lets gooo

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 
 var hoverText = "<h2>Hover over to the circle to continue hacking</h2>"
-var questionText = "<h2>Press the keys that you see in order.</h2>"
+var questionText = "<h2>Press the keys in order. It will display again if it's wrong.</h2>"
 var visibleMaze = false;
 var elementPos;
 var idName;
@@ -69,74 +69,59 @@ function question() {
 
     console.log(puzzleWord);
 
-    // Do I want it to display different?
     puzzleWordHtml = "<h3 class=letter id=letter" + idName[1] + ">" + puzzleWord + "</h3>"
     $("#text-para").append(puzzleWordHtml);
-    $("#letter" + idName[1]).delay(2500).fadeToggle();
+    if (idName[1] == 2) {
+        $("#letter" + idName[1]).delay(4500).fadeToggle();
+    }
+    else {
+        $("#letter" + idName[1]).delay(2500).fadeToggle();
+    }
 
     $(document).on("keypress", function (e) {
         console.log(puzzleWord);
         if (answer.length != puzzleWord.length) {
             answer += e.key;
             console.log(answer);
-            //add input so user knows key input was taken
+            $(".black-background").css("box-shadow", "1em 0 .4em #006602, -1em 0 .4em #006602");
+            setTimeout(function () {
+                $(".black-background").css("box-shadow", "none");
+            }, 300);
         }
         if (answer == puzzleWord) {
+            questionsAnswered[idName[1]] = true;
+
             $("#text-para h2").remove();
             hoverCircle(idName);
 
-            questionsAnswered[idName[1]] = true;
-            console.log(questionsAnswered[idName[1]]);
+            console.log(questionsAnswered[0], questionsAnswered[1], questionsAnswered[2], questionsAnswered[3], questionsAnswered[4])
+            if (questionsAnswered[0] == true && questionsAnswered[1] == true && questionsAnswered[2] == true && questionsAnswered[3] == true && questionsAnswered[4] == true) {
+                $(".door").remove();
+            }
 
             $("#" + idName).remove();
 
-            //show they got it right
-            console.log("Solved!");
+            $(".black-background").css("box-shadow", "1em 0 .4em #00D404, -1em 0 .4em #00D404");
+            setTimeout(function () {
+                $(".black-background").css("box-shadow", "none");
+            }, 300);
+
             $(document).unbind("keypress");
+
         } else if (answer.length == puzzleWord.length) {
-            //show they got it wrong
-            console.log("Wrong!");
+            $(".black-background").css("box-shadow", "1em 0 .4em red, -1em 0 .4em red");
+            setTimeout(function () {
+                $(".black-background").css("box-shadow", "none");
+            }, 800);
 
             $("#letter" + idName[1]).fadeToggle();
             $("#letter" + idName[1]).delay(2500).fadeToggle();
             answer = "";
         }
     });
-    console.log(questionsAnswered[0], questionsAnswered[1], questionsAnswered[2], questionsAnswered[3], questionsAnswered[4])
-    if (questionsAnswered[0] == true && questionsAnswered[1] == true && questionsAnswered[2] == true && questionsAnswered[3] == true && questionsAnswered[4] == true) {
-        $(".door").remove();
-    }
 }
 
 
 $(".maze, .door").on("mouseover", function () {
     console.log("Touching");
 });
-
-
-
-
-
-// function displayString(letterChar, index) {
-//     setTimeout(function () {
-//         $("#text-para").append(letterChar);
-//         $("#letter" + index).fadeToggle();
-//         $("#letter" + index).fadeToggle();
-//         $("#letter" + index).remove();
-//     }, 1000 * index);
-// }
-
-// function displayString(stringName) {
-//     var printNextLetter = function () {
-//         letter = "<h2 class='letter' id=letter" + index + ">" + stringName[index] + "</h2>"
-//         console.log(letter)
-//         if (index < stringName.length) {
-//             $("#text-para").append(letter);
-//             setTimeout(printNextLetter, 1000 * index);
-//             index++;
-//         }
-//         $("#letter" + index).fadeToggle();
-//     }
-//     console.log("#letter" + index)
-//     printNextLetter();
-// }
